d476fa2aae94c1139f4165be6a9c088880c1b714
Lily Pandey <panlily2002@gmail.com>
2025-10-30 15:10:23 +0530

    tests

--- analytics-service/src/test/java/com/pm/analyticsservice/AnalyticsServiceApplicationTests.java
+++ analytics-service/src/test/java/com/pm/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pm.authservice;
+package com.pm.analyticsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+class AnalyticsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
